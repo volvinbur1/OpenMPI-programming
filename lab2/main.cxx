a352b642d808df27bf9e9200a28a02dc35b99671
Volodymyr Burdeinyi <volodymyr.burdeinyi@dxc.com>
2023-06-18 19:48:43 +0300

    * expanded template

--- lab2/main.cxx
+++ lab2/main.cxx
@@ -1,3 +1,34 @@
-int main(int argc, char *argv[]) {
+#include <mpi.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <iomanip>
+#include <array>
+
+std::array<std::string, 3> get_parameters(int curr_rank, int nodes_cnt) {
+    
+    return std::array<std::string, 3>();
+}
+
+double calculate_integral(int curr_rank, int nodes_cnt, std::array<std::string, 3> params) {
     return 0;
+}
+
+int main(int argc, char *argv[]) {
+        MPI_Init(&argc, &argv);
+
+        int curr_rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &curr_rank);
+
+        int nodes_cnt;
+        MPI_Comm_size(MPI_COMM_WORLD, &nodes_cnt);
+
+        std::cout << "[MPI_INIT_INFO] Current node rank: " << curr_rank << "\tTotal nodes count: " << nodes_cnt << std::endl;
+
+        auto params = get_parameters(curr_rank, nodes_cnt);
+        auto result = calculate_integral(curr_rank, nodes_cnt, params);
+
+        MPI_Finalize();
+        return 0;
 }
